979c1c2adeb881c377b9a84762a0a7492a02f5c5
Mohit Kambli <mohitkambli8@gmail.com>
2025-02-20 13:19:33 -0500

    Updated Todo Model

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type Todo struct {
+	gorm.Model
 	ID          uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
